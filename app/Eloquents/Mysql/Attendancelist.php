63c23913e45e24e6effb061fa0aa141384ba637f
m-ogushi <s1520623@u.tsukuba.ac.jp>
2017-05-12 11:24:58 +0000

    データ取得の際の、記述を改善しました

--- app/Eloquents/Mysql/Attendancelist.php
+++ app/Eloquents/Mysql/Attendancelist.php
@@ -2,15 +2,12 @@
 
 namespace App\Eloquents\Mysql;
 
-use Illuminate\Support\Facades\DB;
-
 class Attendancelist extends Mysql
 {
     protected $table = 'attendancelist';
 
      static function get_data()
      {
-         $users = DB::select('select * from attendancelist');
-         return $users;
+         return self::get();
      }
 }
