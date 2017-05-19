e0b1e14f42ba27e71252e5e0b752bc6c6b264734
m-ogushi <s1520623@u.tsukuba.ac.jp>
2017-05-19 10:11:28 +0000

    「スピーチする」が連続クリックされた際に、複数回カウントされないようにするための記述

--- app/Eloquents/Mysql/AttendanceList.php
+++ app/Eloquents/Mysql/AttendanceList.php
@@ -6,31 +6,31 @@ class AttendanceList extends Mysql
 {
     protected $table = 'attendancelist';
 
-     static function decideMenber( $already )
-     {
-         $query = self::take(1)->orderByRaw( 'RAND()' );
-         if( $already == 1 ){
-             return $query->where( 'end', 0 )->get();
-         }
-         return $query->get();
-     }
+    static function decideMenber( $already )
+    {
+        $query = self::take(1)->orderByRaw( 'RAND()' );
+        if( $already == 1 ){
+            return $query->where( 'end', 0 )->get();
+        }
+        return $query->get();
+    }
 
 
-     static function confirmMember( $id,$cancel )
-     {
-         if( $cancel == 1 )
-         {
-             Self::where( 'id','=',$id )->decrement( 'end',1 );
-         }else
-         {
-             Self::where( 'id','=',$id )->increment( 'end',1 );
-         }
-     }
+    static function confirmMember( $id,$cancel )
+    {
+        if( $cancel == 1 )
+        {
+            Self::where( 'id','=',$id )->decrement( 'end',1 );
+        }else
+        {
+            Self::where( 'id','=',$id )->increment( 'end',1 );
+        }
+    }
     
     
-     static function resetMember()
-     {
-         //現在実装中
-         Self::where( 'end', "<>" ,0 )->update( [ 'end'=>0 ] );
-     }
+    static function resetMember()
+    {
+        //現在実装中
+        Self::where( 'end', "<>" ,0 )->update( [ 'end'=>0 ] );
+    }
 }
